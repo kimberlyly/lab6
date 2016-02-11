e9d7218036f6fc9bb9868da51b68038630191eb5
Kimberly Ly <kimberly.ly95@gmail.com>
2016-02-11 09:19:26 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,14 +25,31 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
+	$.get('project/' + idNumber, callback);
 	console.log("User clicked on project " + idNumber);
+	console.log("project/" + idNumber);
 }
 
+function callback(result) {
+	console.log(result);
+	var projectCode = '<img src="' + result['image'] + '" class="detailsImage">' + 
+		              '<p><small>' + result['date'] + '</small></p>' + result['summary'];
+	$("#project" + result.id + " .details").html(projectCode);
+}
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette', colorCallback);
+}
+
+function colorCallback(result) {
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
